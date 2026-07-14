9834631b49560dd919895a836a7d495cea8e52a4
Mohamed jbeli <jbelimohamed2025@gmail.com>
2026-07-14 16:36:08 +0100

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GmaoProvider } from './context/GmaoContext.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GmaoProvider>
       <App />
+      <Analytics />
     </GmaoProvider>
   </StrictMode>,
 )
